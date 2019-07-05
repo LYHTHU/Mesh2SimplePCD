94b0981bec41af3c74699600919962c05e01862d
yunhaoli <yunhaoli.cs@gmail.com>
2019-07-05 16:34:50 -0400

    Working with CGAL and libigl

--- MeshSimplify.cpp
+++ MeshSimplify.cpp
@@ -9,3 +9,15 @@ MeshSimplify::MeshSimplify(string path, bool visible) {
     this->visible = visible;
 }
 
+MeshSimplify::MeshSimplify(Polyhedron *obj, bool visible) {
+    this->mesh = obj;
+    this->visible = visible;
+}
+
+void MeshSimplify::simplify(int num_points, int num_faces) {
+
+}
+
+void MeshSimplify::save(string out_path) {
+
+}
--- MeshSimplify.h
+++ MeshSimplify.h
@@ -5,16 +5,29 @@
 #ifndef MESH2SIMPLEPCD_MESHSIMPLIFY_H
 #define MESH2SIMPLEPCD_MESHSIMPLIFY_H
 
+
 #include <iostream>
 #include <string>
+#include <CGAL/Polyhedron_3.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Polyhedron_3<Kernel>     Polyhedron;
 
 using namespace std;
+using namespace CGAL;
+
 
 class MeshSimplify {
 public:
     string path;
     bool visible;
-    MeshSimplify(string path, bool visible = false);
+    Polyhedron *mesh = nullptr;
+
+    MeshSimplify(string path="", bool visible = false);
+    MeshSimplify(Polyhedron *obj, bool visible);
+
+    void simplify(int num_points, int num_faces);
+    void save(string out_path);
 
 };
 
